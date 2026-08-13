31a78f0a7d821e2ce38a6d47e27cb862f749f1fc
thomaspanesso-creator <thomaspanesso@gmail.com>
2026-08-13 17:20:13 -0500

    Add files via upload

--- apiusuarios/src/main/java/com/example/apiusuarios/ApiusuariosApplication.java
+++ apiusuarios/src/main/java/com/example/apiusuarios/ApiusuariosApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiusuariosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiusuariosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiusuariosApplication.class, args);
+    }
 
 }
